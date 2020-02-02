808fe4bf329b53976aae4b0f72cb4f9123401e9d
brunnermanu <brunner.manu@hotmail.com>
2020-02-02 08:13:39 +0100

    add list area

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListAreaComponent} from './listArea/listArea.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-
+    ListAreaComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/listArea/listArea.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-area',
+  templateUrl: './listArea.component.html',
+  styleUrls: ['./listArea.component.scss']
+})
+
+export class ListAreaComponent {
+
+}
