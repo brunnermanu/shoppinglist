95ec5723f2beae1bac7ae0144fb3696b3afc12b7
brunnermanu <brunner.manu@hotmail.com>
2020-04-03 08:10:13 +0200

    add updates

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,6 +10,9 @@ export class RecipeListComponent implements OnInit {
     new Recipe('A Test Recipe', 'This is simply a Test',
       'https://res.cloudinary.com/swissmilk/image/fetch/q_auto,f_auto/' +
       'https://api.swissmilk.ch/wp-content/uploads/2019/05/ss-alltagsrezepte-winter-geschmortes-wintergemuese-mit-poulet.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a Test',
+      'https://res.cloudinary.com/swissmilk/image/fetch/q_auto,f_auto/' +
+      'https://api.swissmilk.ch/wp-content/uploads/2019/05/ss-alltagsrezepte-winter-geschmortes-wintergemuese-mit-poulet.jpg'),
   ];
   constructor() { }
 
