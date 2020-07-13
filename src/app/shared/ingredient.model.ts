bc76df2dcef59d83618f6ae41cb74f9e30cb29e1
brunnermanu <brunner.manu@hotmail.com>
2020-07-13 08:08:10 +0200

    add buttonfunctions

--- src/app/shared/ingredient.model.ts
+++ src/app/shared/ingredient.model.ts
@@ -1,3 +1,16 @@
 export class Ingredient {
-  constructor(public amount: number, public unit: string, public name: string) {}
+  constructor(public amount: number, public unit: string, public name: string, public category: string) {}
 }
+
+
+
+// import { Category } from '../shared/category.model';
+//
+// export class Ingredient {
+//   constructor(public amount: number, public unit: string, public name: string, public category: Category) {
+//     this.amount = amount;
+//     this.unit = unit;
+//     this.name = name;
+//     this.category = category;
+//   }
+// }
