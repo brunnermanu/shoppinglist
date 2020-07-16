5a390341fa0dcfede66a753a16506496176426d6
brunnermanu <brunner.manu@hotmail.com>
2020-07-16 17:24:38 +0200

    add category sort

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,42 +1,78 @@
 import { Ingredient } from '../shared/ingredient.model';
 import { Subject } from 'rxjs';
+import { CategoryService } from '../category-edit/category.service';
+import { Injectable } from '@angular/core';
 
-
+@Injectable()
 export class ShoppingListService {
   ingredientsChanged = new Subject<Ingredient[]>();
   startedEditing = new Subject<number>();
-  categories = ['Früchte','Gemüse', 'Brot', 'Milchprodukte', 'Fleisch', 'Mehl', 'Teigwaren', 'Dosen', 'Eingemachtes', 'Pflegeprodukte', 'Putzmittel', 'Sonstiges'];
+  // Improve plural naming
+  categories: string[];
 
-  private ingredients: Ingredient[] = [
-    new Ingredient(5, 'stk', 'Äpfel', 'Früchte'),
-    new Ingredient(200, 'gr', 'Spaghetti', 'Teigwaren'),
+  testCategories = [
+    'Früchte',
+    'Gemüse',
+    'Brot',
+    'Milch',
+    'Fleisch',
+    'Backen',
+    'Teigwaren',
+    'Sonstiges',
   ];
 
-getIngredients() {
-    return this.ingredients.sort((a, b) => this.categories.indexOf(a.category) - this.categories.indexOf(b.category)).slice();
+  constructor(private categoryService: CategoryService) {
+
+    this.categories = this.categoryService.categories.map(item => item.category);
+    console.log('this.categories', this.categories);
+    console.log('this.testCategories', this.testCategories);
   }
 
-getIngredient(index: number) {
+  // category array von vorhin nochmals hierhin schreiben und die Lösungen vergleichen
+
+
+  private ingredients: Ingredient[] = [
+    new Ingredient(200, 'gr', 'Spaghetti', 'Teigwaren'),
+    new Ingredient(5, 'stk', 'Äpfel', 'Früchte'),
+  ];
+
+  getIngredients() {
+    return this.ingredients.sort(
+      (a, b) =>
+        this.categories.indexOf(a.category) - this.categories.indexOf(b.category)
+    ).slice();
+  }
+
+  getIngredient(index: number) {
     return this.ingredients[index];
   }
 
-addIngredient(ingredient: Ingredient) {
+  addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.next(this.ingredients.sort((a, b) => this.categories.indexOf(a.category) - this.categories.indexOf(b.category)).slice());
+    this.ingredientsChanged.next(this.ingredients.sort(
+      (a, b) =>
+        this.categories.indexOf(a.category) - this.categories.indexOf(b.category))
+      .slice());
   }
 
-addIngredients(ingredients: Ingredient[]) {
+  addIngredients(ingredients: Ingredient[]) {
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.next(this.ingredients.sort((a, b) => this.categories.indexOf(a.category) - this.categories.indexOf(b.category)).slice());
+    this.ingredientsChanged.next(this.ingredients.sort(
+      (a, b) =>
+        this.categories.indexOf(a.category) - this.categories.indexOf(b.category))
+      .slice());
   }
 
-updateIngredient(index: number, newIngredient: Ingredient) {
+  updateIngredient(index: number, newIngredient: Ingredient) {
     this.ingredients[index] = newIngredient;
-    this.ingredientsChanged.next(this.ingredients.sort((a, b) => this.categories.indexOf(a.category) - this.categories.indexOf(b.category)).slice());
+    this.ingredientsChanged.next(this.ingredients.sort(
+      (a, b) =>
+        this.categories.indexOf(a.category) - this.categories.indexOf(b.category))
+      .slice());
   }
 
-deleteIngredient(index: number) {
-  this.ingredients.splice(index, 1);
-  this.ingredientsChanged.next(this.ingredients.sort((a, b) => this.categories.indexOf(a.category) - this.categories.indexOf(b.category)).slice());
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
