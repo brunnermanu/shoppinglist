8571accc369fe3496ee21c16e9ea4f30e12b0fa1
brunnermanu <brunner.manu@hotmail.com>
2020-08-11 14:00:35 +0200

    add login functions to switch from signup to login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,9 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import {CategoryEditComponent} from './category-edit/category-edit.component';
+import { CategoryEditComponent } from './category-edit/category-edit.component';
+import { RecipesResolverService } from './recipes/recipes-resolver.service';
+import { AuthComponent } from './auth/auth.component';
 
 
 
@@ -16,9 +18,10 @@ const appRoutes: Routes = [
   { path: 'recipes', component: RecipesComponent, children: [
       { path: '', component: RecipeStartComponent },
       { path: 'new', component: RecipeEditComponent },
-      { path: ':id', component: RecipeDetailComponent },
-      { path: ':id/edit', component: RecipeEditComponent },
+      { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService] },
+      { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService] },
     ]},
+  { path: 'auth', component: AuthComponent}
 ];
 
 @NgModule({
